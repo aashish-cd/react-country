c409e566814f98798cff14ff9e0a27b1e1313a30
aashish-cd <076bct002@ioepc.edu.np>
2021-10-04 23:08:00 +0545

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState('nep');
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+
+    return () => {};
+  }, [name]);
+  const fetchData = async () => {
+    // setLoading(true);
+    const response = await axios.get(
+      `https://restcountries.com/v3.1/name/${name}`
+    );
+    // console.log(response.data);
+    setData(response.data);
+    console.log(data);
+    // setLoading(false);
+  };
+
+  //handle search name
+  const handleName = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+  };
+
+  if (loading) {
+    return (
+      <div>
+        <h2>Loading... </h2>
+        <div className='underline'></div>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='section'>
+      <form>
+        <input type='text' value={name} onChange={(e) => handleName(e)} />
+      </form>
+      <div className='container'>
+        {data.map((ss, index) => {
+          return <h1 key='index'>{ss.name.common}</h1>;
+        })}
+      </div>
+    </section>
   );
 }
 
